3b3d5b38ed46c5951a1033abc7b66e0eb61fad8d
Pol Llagostera Blasco <plb92@hotmail.com>
2023-01-11 18:35:11 +0100

    First commit

---
+++ name/windows/name/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::name::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::name::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
